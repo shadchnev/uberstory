d290f41a6398799c1b052ab4e1f93ca8f7479e01
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-05-24 14:50:42 +0100

    merge of start of the markup

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -4,4 +4,8 @@ module StoriesHelper
     original.gsub(/\w/) { ('a'..'z').to_a[rand(26)] }
   end
   
+  def can_add_new_line(user, story)
+    story.involves?(current_user) && !story.last_line_by?(user)
+  end
+  
 end
