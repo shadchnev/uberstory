285e310379e88e1a2d7f301837bfe453e69f90c5
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-05-27 16:18:46 +0100

    fixing app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Uberstory::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.host_url = "http://apps.facebook.com/uberstory"
+  config.host_url = "http://apps.facebook.com/ubertales"
   
 end
