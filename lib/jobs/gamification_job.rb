dca92ee9b4d35f5decc1c239367b1a74aff9608b
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-06-27 09:43:49 +0100

    switching from the delayed job

--- lib/jobs/gamification_job.rb
+++ lib/jobs/gamification_job.rb
@@ -2,7 +2,8 @@ class GamificationJob < Struct.new(:event, :payload)
   
   def perform
     Rules::Rule.filter(event, payload).each do |rule|
-      Delayed::Job.enqueue CheckRuleJob.new(rule, event, payload)
+      # Delayed::Job.enqueue CheckRuleJob.new(rule, event, payload)
+      CheckRuleJob.new(rule, event, payload).perform
     end
   end
   
