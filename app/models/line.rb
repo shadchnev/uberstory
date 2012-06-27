5f72c2dcdef3e360e814bcc03dcecf72b02d822d
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-06-27 11:40:09 +0100

    you can't read the story unless it's finished

--- app/models/line.rb
+++ app/models/line.rb
@@ -9,8 +9,9 @@ class Line < ActiveRecord::Base
   validates_length_of :text, :maximum => 140
   
   def visible_to?(user)
-    last_line_by_user = story.lines.find_all_by_user_id(user.id).last
-    new_record? || (last_line_by_user and created_at and created_at <= last_line_by_user.created_at) || story.lines.last == self || !story.involves?(user)
+    # last_line_by_user = story.lines.find_all_by_user_id(user.id).last
+    # new_record? || (last_line_by_user and created_at and created_at <= last_line_by_user.created_at) || story.lines.last == self || !story.involves?(user)
+    story.finished? || story.lines.last == self
   end
   
   def number
